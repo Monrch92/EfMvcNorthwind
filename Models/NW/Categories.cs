c91985747fe9f498bb655ab165e2573382d44347
Monrch92 <wchang67@my.bcit.ca>
2020-09-23 01:09:52 -0700

    1st commit

---
+++ Models/NW/Categories.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfMvcNorthWind.Models.NW
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+
+        [Required (ErrorMessage = "You must enter a {0}")]
+        [MaxLength(15)]
+        [Display (Name = "Category")]
+        public string CategoryName { get; set; }
+
+        [Required]
+        [MaxLength(16)]
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
